f050c15c1ad55e80a3a5c71e01988fd22cf53d55
wael <wael.bannani@esprit.tn>
2022-10-04 17:09:58 +0100

    facture

--- src/main/java/com/esprit/examen/entities/Facture.java
+++ src/main/java/com/esprit/examen/entities/Facture.java
@@ -44,5 +44,15 @@ public class Facture implements Serializable {
     @JsonIgnore
     private Set<Reglement> reglements;
 
+	public Facture(Date dateCreationFacture , Date dateDerniereModificationFacture , Boolean archivee, Set<DetailFacture> detailsFacture,Fournisseur fournisseur,Set<Reglement> reglements ) {
+		super();
+		this.dateCreationFacture = dateCreationFacture;
+		this.dateDerniereModificationFacture = dateDerniereModificationFacture;
+		this.archivee = archivee;
+		this.fournisseur = fournisseur;
+		this.detailsFacture =detailsFacture;
+		this.reglements = reglements;
+
+	}
 	
 }
