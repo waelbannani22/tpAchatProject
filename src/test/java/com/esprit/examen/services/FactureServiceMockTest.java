c89a62a861971198f8fc68f5668ac91a1b914249
wael <wael.bannani@esprit.tn>
2022-11-02 00:15:19 +0100

    movck

--- src/test/java/com/esprit/examen/services/FactureServiceMockTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceMockTest.java
@@ -23,7 +23,7 @@ import com.esprit.examen.repositories.FactureRepository;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-public class FactureServiceMock {
+public class FactureServiceMockTest {
 	
 	
 	@InjectMocks
