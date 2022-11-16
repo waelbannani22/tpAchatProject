8406d19893b678bf968e1aaaa5f911c8e033b82a
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 14:46:38 +0100

    Update ReglementServiceTest.java

--- src/test/java/com/esprit/examen/services/ReglementServiceTest.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceTest.java
@@ -1,1 +1,31 @@
 
+package com.esprit.examen.services;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.esprit.examen.entities.Facture;
+import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Reglement;
+import com.esprit.examen.entities.Stock;
+import com.esprit.examen.repositories.ReglementRepository;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class ReglementServiceTest {
+@Autowired
+IReglementService reglementservice;
+
+  }
+  
