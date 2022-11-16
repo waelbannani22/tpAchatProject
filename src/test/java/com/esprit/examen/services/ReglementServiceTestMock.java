2df85e5260ab071d0194521e087c94b2cc4b0ca6
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 15:05:47 +0100

    Update ReglementServiceTestMock.java

--- src/test/java/com/esprit/examen/services/ReglementServiceTestMock.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceTestMock.java
@@ -1,1 +1,6 @@
-
+package com.esprit.examen.services;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
