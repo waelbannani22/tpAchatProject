3c21eee94ca38e947e38e16e0fa56f9f83364d8f
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 15:02:44 +0100

    Update ReglementServiceTest.java

--- src/test/java/com/esprit/examen/services/ReglementServiceTest.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceTest.java
@@ -84,4 +84,5 @@ assertNull(reglementservice.retrieveReglement(id));
     Facture s = new Facture(dateNaissance, dateNaissance, false, null, null, null);
      R.setFacture(s);
     reglementservice.addReglement(R);
+     assertNotNull
 }
