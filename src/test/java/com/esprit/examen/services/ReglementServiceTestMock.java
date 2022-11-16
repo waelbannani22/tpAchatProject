7c684e2ba0ddcb7840fbd549b9c34e57fc6455be
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 15:09:19 +0100

    Update ReglementServiceTestMock.java

--- src/test/java/com/esprit/examen/services/ReglementServiceTestMock.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceTestMock.java
@@ -18,3 +18,4 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
