7f3da6c54401472dd335206888e589aaf89f0dd6
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 14:51:56 +0100

    Update ReglementServiceTest.java

--- src/test/java/com/esprit/examen/services/ReglementServiceTest.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceTest.java
@@ -51,4 +51,9 @@ assertNull(reglementservice.retrieveReglement(id));
 }
    @Test
 public void retrieveReglementByFacture()throws ParseException {
+  SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+   Date dateNaissance = dateFormat.parse("25/09/2000");
+    Facture s = new Facture(dateNaissance, dateNaissance, false, null, null, null);
+     Date Date1 = dateFormat.parse("25/09/2000");
+    Reglement R = new Reglement(22, 22, false, Date1);
 }
