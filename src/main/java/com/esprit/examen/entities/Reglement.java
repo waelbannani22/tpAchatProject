ddab8545787374ae0d5f34119c9d950e46382840
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 15:42:06 +0100

    Update Reglement.java

--- src/main/java/com/esprit/examen/entities/Reglement.java
+++ src/main/java/com/esprit/examen/entities/Reglement.java
@@ -22,6 +22,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Reglement implements Serializable{
 private static final long serialVersionUID = 1L;
+	@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 
 
