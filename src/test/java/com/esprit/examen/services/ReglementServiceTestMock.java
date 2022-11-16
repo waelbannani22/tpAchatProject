cb24dcd662838d7c51de6f54fae2883ec1a4d2f3
raniasellami <47666671+raniasellami@users.noreply.github.com>
2022-11-16 15:09:57 +0100

    Update ReglementServiceTestMock.java

--- src/test/java/com/esprit/examen/services/ReglementServiceTestMock.java
+++ src/test/java/com/esprit/examen/services/ReglementServiceTestMock.java
@@ -20,3 +20,6 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+
+import com.esprit.examen.entities.Facture;
