27e250592681f2209cd68a70ec0f48f84564c4a3
wael <wael.bannani@esprit.tn>
2022-11-17 15:52:45 +0100

    oo

--- src/test/java/com/esprit/examen/services/FactureServiceMockTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceMockTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
