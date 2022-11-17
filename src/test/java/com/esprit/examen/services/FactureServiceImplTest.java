751e0b2a4eea72c3e12da2841c2d2e858f96185a
wael <wael.bannani@esprit.tn>
2022-11-17 15:46:09 +0100

    Update FactureServiceImplTest.java

--- src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FactureServiceImplTest.java
@@ -1,20 +1,17 @@
 package com.esprit.examen.services;
-
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import javax.transaction.Transactional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.esprit.examen.entities.Facture;
